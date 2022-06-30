6de728c9a102215a38c6257d0c49de146e0c9d87
taeyoung1472 <77605585+taeyoung1472@users.noreply.github.com>
2022-06-30 19:45:22 +0900

    오브 스포너/오브

--- Assets/01. Scripts/Orb/OrbShooter.cs
+++ Assets/01. Scripts/Orb/OrbShooter.cs
@@ -6,13 +6,28 @@ public class OrbShooter : MonoBehaviour
 {
     [SerializeField] private Orb orb;
     [SerializeField] private Transform spawnPos;
-    public void SpawnOrb()
+    bool isShoot;
+
+    public void Start()
     {
-        Instantiate(orb, spawnPos.position, Quaternion.identity);
+        ActiveOrb();
     }
-
-    public void DespawnOrb()
+    public void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.Space) && !isShoot)
+        {
+            Shoot();
+        }
+    }
+    public void ActiveOrb()
+    {
+        orb.transform.position = spawnPos.position;
+        orb.gameObject.SetActive(true);
+        isShoot = false;
+    }
+    public void Shoot()
+    {
+        isShoot = true;
+        orb.Shoot(() => ActiveOrb());
     }
 }
